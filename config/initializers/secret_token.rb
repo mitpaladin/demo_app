94bf7f4a8ca7f47d99bd162d9c3e8c6184748876
Charles Du <charles.q.du@theprolog.com>
2014-08-29 17:10:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '26461781550bb1caee89721364be56929d5759152ff1b495f0b9a7f1da1f0a73c77d99b8eb8d49435074f74f1cc11e55f657d18dcb2a1264bd0de8e4687cfac7'
